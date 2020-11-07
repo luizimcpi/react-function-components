f34950c4507cef55b03329cbfea6708e2642fdfb
Luiz Henrique <luizhenrique.se@gmail.com>
2020-11-07 18:40:58 -0300

    instalando fonte customizada e melhorando header h1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
-import {Container} from "@material-ui/core"
+import {Container, Typography} from "@material-ui/core"
+import 'fontsource-roboto'
 
 class App extends Component {
   render(){
     return (
       <Container component="article" maxWidth="sm">
-        <h1>React Function Components</h1>
+        <Typography variant="h3" component="h1" align="center">React Function Components</Typography>
         <FormularioCadastro />
       </Container>
     );
