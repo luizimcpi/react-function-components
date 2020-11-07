ee5d6a2a37c3692ec3e8ae48532b5fae91bb620f
Luiz Henrique <luizhenrique.se@gmail.com>
2020-11-07 18:41:37 -0300

    alterando texto do h1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render(){
     return (
       <Container component="article" maxWidth="sm">
-        <Typography variant="h3" component="h1" align="center">React Function Components</Typography>
+        <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
         <FormularioCadastro />
       </Container>
     );
