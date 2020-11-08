910e9fefd5ab71212784c6f2bfcc06243e58d104
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-11-07 23:51:57 -0300

    adicionando onchange onsubmit rules ao form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {TextField, Button, Switch, FormControlLabel}  from "@material-ui/core"
 
+
 function FormularioCadastro(){
+
+    const [nome, setNome] = useState("")
     return(
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
+        <form 
+        onSubmit={
+            (event) => {
+                event.preventDefault();
+                console.log(nome);
+            }
+        }>
+            <TextField 
+                value={nome}
+                onChange={
+                     event => {
+                         setNome(event.target.value);
+                        }
+                    }
+                id="nome" 
+                label="Nome" 
+                variant="outlined" 
+                margin="normal" 
+                fullWidth
+            />
         
-            <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
+            <TextField 
+                id="sobrenome" 
+                label="Sobrenome" 
+                variant="outlined" 
+                margin="normal" 
+                fullWidth
+            />
 
-            <TextField id="cpf" label="CPF" variant="outlined"  margin="normal" fullWidth/>
+            <TextField 
+                id="cpf" 
+                label="CPF" 
+                variant="outlined"  
+                margin="normal" 
+                fullWidth
+            />
 
-            <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary"/>}/>
+            <FormControlLabel 
+                label="Promoções" 
+                control={<Switch name="promocoes" defaultChecked color="primary"/>}
+            />
 
-            <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked color="primary"/>}/>
+            <FormControlLabel 
+                label="Novidades" 
+                control={<Switch name="novidades" defaultChecked color="primary"/>}
+            />
             
-            <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
+            <Button 
+                variant="contained" 
+                color="primary" 
+                type="submit">Cadastrar</Button>
         </form>
     );
 }
