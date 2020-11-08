541d2d42afb3e7a2b097ac2e6e52f738d6352eb2
Luiz Henrique <luizhenrique.se@gmail.com>
2020-11-08 17:01:44 -0300

    adicionando hooks aos elementos de cpf e checkboxes do form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,72 +1,83 @@
-import {React, useState} from 'react';
-import {TextField, Button, Switch, FormControlLabel}  from "@material-ui/core"
+import { React, useState } from 'react';
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core"
 
 
-function FormularioCadastro(){
+function FormularioCadastro() {
 
     const [nome, setNome] = useState("")
     const [sobrenome, setSobrenome] = useState("")
-    return(
-        <form 
-        onSubmit={
-            (event) => {
-                event.preventDefault();
-                console.log(nome, sobrenome);
-            }
-        }>
-            <TextField 
+    const [cpf, setCpf] = useState("")
+    const [promocoes, setPromocoes] = useState(true)
+    const [novidades, setNovidades] = useState(true)
+    return (
+        <form
+            onSubmit={
+                (event) => {
+                    event.preventDefault();
+                    console.log(nome, sobrenome, cpf, promocoes, novidades);
+                }
+            }>
+            <TextField
                 value={nome}
                 onChange={
-                     event => {
-                         let tempNome = event.target.value;
-                         if(tempNome.length >= 3){
-                             tempNome = tempNome.substring(0,3);
-                         }
-                         setNome(tempNome);
-                        }
+                    event => {
+                        setNome(event.target.value);
                     }
-                id="nome" 
-                label="Nome" 
-                variant="outlined" 
-                margin="normal" 
+                }
+                id="nome"
+                label="Nome"
+                variant="outlined"
+                margin="normal"
                 fullWidth
             />
-        
-            <TextField 
+
+            <TextField
                 value={sobrenome}
                 onChange={
                     event => {
                         setSobrenome(event.target.value);
-                        }
                     }
-                id="sobrenome" 
-                label="Sobrenome" 
-                variant="outlined" 
-                margin="normal" 
+                }
+                id="sobrenome"
+                label="Sobrenome"
+                variant="outlined"
+                margin="normal"
                 fullWidth
             />
 
-            <TextField 
-                id="cpf" 
-                label="CPF" 
-                variant="outlined"  
-                margin="normal" 
+            <TextField
+                value={cpf}
+                onChange={
+                    event => {
+                        setCpf(event.target.value);
+                    }
+                }
+                id="cpf"
+                label="CPF"
+                variant="outlined"
+                margin="normal"
                 fullWidth
             />
 
-            <FormControlLabel 
-                label="Promoções" 
-                control={<Switch name="promocoes" defaultChecked color="primary"/>}
+            <FormControlLabel
+                label="Promoções"
+                control={<Switch checked={promocoes} onChange={(event => {
+                    setPromocoes(event.target.checked)
+                })}
+                    name="promocoes" color="primary" />}
             />
 
-            <FormControlLabel 
-                label="Novidades" 
-                control={<Switch name="novidades" defaultChecked color="primary"/>}
+            <FormControlLabel
+                label="Novidades"
+                control={<Switch checked={novidades} onChange={(event => {
+                    setNovidades(event.target.checked)
+                })}
+                    name="novidades" color="primary" />}
             />
-            
-            <Button 
-                variant="contained" 
-                color="primary" 
+
+            <Button
+                variant="contained"
+                color="primary"
                 type="submit">Cadastrar</Button>
         </form>
     );
